aad426c219aaa1f5b4c79c52b4c77ed4d9eff502
jcheung <jacheung6@gmail.com>
2023-03-09 11:23:34 -0800

    leverage GPU acceleration for training

--- app/ml/model.py
+++ app/ml/model.py
@@ -3,7 +3,8 @@ import time
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
-import preprocessing
+import tensorflow_datasets as tfds
+from app.ml import preprocessing
 
 
 class Model:
@@ -12,36 +13,38 @@ class Model:
         self._model_path = model_path
         self.load()
 
-    def train(self, X: np.ndarray, y: np.ndarray):
-        learning_rate = 0.001
-        l1 = 0.
-        l2 = 0.
-        num_hidden = 16
-        regularizer = tf.keras.regularizers.l1_l2(l1, l2)
-        class_names = np.unique(y)
+    def train(self, xy_tuple):
+        # # GPU acceleration (still in development)
+        gpus = tf.config.list_logical_devices('GPU')
+        strategy = tf.distribute.MirroredStrategy(gpus)
+        with strategy.scope():
+            learning_rate = 0.01
+            l1 = 0.
+            l2 = 0.
+            num_hidden = 16
+            regularizer = tf.keras.regularizers.l1_l2(l1, l2)
 
-        layers = [
-            hub.KerasLayer(
-                "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4",
-                input_shape=tuple(x_train_processed.shape[1:]),
-                trainable=False,
-                name='mobilenet_embedding'),
-            tf.keras.layers.Dense(num_hidden,
-                                  kernel_regularizer=regularizer,
-                                  activation='relu',
-                                  name='dense_hidden'),
-            tf.keras.layers.Dense(len(class_names),
-                                  kernel_regularizer=regularizer,
-                                  activation='softmax',
-                                  name='mnist_prob')
-        ]
-
-        self._model = tf.keras.Sequential(layers, name='mnist_classification')
-        self._model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-                            loss=tf.keras.losses.SparseCategoricalCrossentropy(
-                            from_logits=False),
-                            metrics=['accuracy'])
-        self._model.fit(X, y, epochs=5)
+            layers = [
+                hub.KerasLayer(
+                    "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4",
+                    input_shape=(224, 224, 3),
+                    trainable=False,
+                    name='mobilenet_embedding'),
+                tf.keras.layers.Dense(num_hidden,
+                                      kernel_regularizer=regularizer,
+                                      activation='relu',
+                                      name='dense_hidden'),
+                tf.keras.layers.Dense(len(class_names),
+                                      kernel_regularizer=regularizer,
+                                      activation='softmax',
+                                      name='mnist_prob')
+            ]
+            self._model = tf.keras.Sequential(layers, name='mnist_classification')
+            self._model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+                                loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                                from_logits=False),
+                                metrics=['accuracy'])
+            self._model.fit(xy_tuple, epochs=5)
         return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
@@ -72,7 +75,15 @@ def get_model():
 
 
 if __name__ == "__main__":
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    x_train_processed = preprocessing.preprocess_mnist(x_train)
-    model.train(x_train_processed, y_train)
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+    )
+    class_names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ds_train = ds_train.map(preprocessing.preprocess_mnist_tfds, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_train = ds_train.batch(128)
+    model.train(ds_train)
     model.save()
--- app/ml/preprocessing.py
+++ app/ml/preprocessing.py
@@ -1,11 +1,13 @@
 import numpy as np
 import tensorflow as tf
 
+
 def expand_greyscale_image_channels(grey_pil_image):
     grey_image_arr = np.array(grey_pil_image)
     grey_image_arr = np.expand_dims(grey_image_arr, -1)
     grey_image_arr_3_channel = grey_image_arr.repeat(3, axis=-1)
     return grey_image_arr_3_channel
+
 
 def preprocess_mnist(images: np.array):
     # reshape and upsample to 3 channel for transfer learning models
@@ -15,4 +17,15 @@ def preprocess_mnist(images: np.array):
     # resize with pad for mobilenetv2
     images = tf.image.resize_with_pad(images, target_height=224, target_width=224)
     return images
+
+
+def preprocess_mnist_tfds(image, label):
+    # reshape and upsample to 3 channel for transfer learning models
+    image = tf.image.grayscale_to_rgb(image)
+    # normalize pixel values
+    image = tf.cast(image, tf.float32) / 255.
+    # resize with pad for mobilenetv2
+    image = tf.image.resize_with_pad(image, target_height=224, target_width=224)
+    return image, label
+
 
