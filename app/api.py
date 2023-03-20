4c60f7652236510c349ae100380d013ee4ee0453
jcheung <jacheung6@gmail.com>
2023-03-20 15:28:51 -0700

    progress on patching upload file jpeg

--- app/api.py
+++ app/api.py
@@ -7,30 +7,32 @@ from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
 from app.ml.model import Model, get_model
 import matplotlib.image as mpimg
 
-router = APIRouter(prefix='/v1/thinMLdeployment')
+app = FastAPI()
 
 
-class PredictRequest(BaseModel):
-    data: int
-
-    # @validator("data")
-    # def check_dimensionality(cls, v):
-    #     for point in v:
-    #         if len(point) != n_features:
-    #             raise ValueError(f"Each data point must contain {n_features} features")
-    #
-    #     return v
-
-
+# class PredictRequest(BaseModel):
+#     data: int
+#
+#     # @validator("data")
+#     # def check_dimensionality(cls, v):
+#     #     for point in v:
+#     #         if len(point) != n_features:
+#     #             raise ValueError(f"Each data point must contain {n_features} features")
+#     #
+#     #     return v
+#
+#
 class PredictResponse(BaseModel):
     data: int
 
 
-@router.post("/predict",
-             description="Predict MNIST image")
-def predict(image: PredictRequest, model: Model = Depends(get_model)):
-    X = mpimg.imread(image)
+@app.post("/predict",
+          description="Predict MNIST image")
+async def predict(file: UploadFile = File(...), model: Model = Depends(get_model)):
+    file.file
+    X = mpimg.imread(file.file)
     y_pred = model.predict_single_image(image=X)
+    print(y_pred)
     result = PredictResponse(data=y_pred.tolist())
 
     return result
